2e5007d75ab3776a8648418073a8d3d5ad7246e6
Josh Young <jyoung@sparkcognition.com>
2019-03-29 13:20:45 -0500

    Pushing a new file to a remote::

---
+++ JY_Work_Jarvis/CODE/Test_git/test_pub.cpp
@@ -0,0 +1,52 @@
+OLD______________________  .h   ===========================
+
+class JoystickEvents
+{
+public:
+    virtual void OnGamePadChanged(const GamePadEventData *evt);
+};
+
+
+
+New____________________  .h   =============================
+class JoystickEvents
+{
+public:
+    virtual void OnGamePadChanged(const GamePadEventData *evt);
+    static GamePadEventData mostRecentEvent;
+};
+
+
+
+
+
+OLD______________________  .cpp   ===========================
+
+void JoystickEvents::OnGamePadChanged(const GamePadEventData *evt)
+{
+    Serial.print("X: ");
+    PrintHex<uint16_t>(evt->x, 0x80);
+    Serial.print(" Y: ");
+    PrintHex<uint16_t>(evt->y, 0x80);
+    Serial.print(" Hat Switch: ");
+    PrintHex<uint8_t>(evt->hat, 0x80);
+    Serial.print(" Twist: ");
+    PrintHex<uint8_t>(evt->twist, 0x80);
+    Serial.print(" Slider: ");
+    PrintHex<uint8_t>(evt->slider, 0x80);
+    Serial.print(" Buttons A: ");
+    PrintHex<uint8_t>(evt->buttons_a, 0x80);
+    Serial.print(" Buttons B: ");
+    PrintHex<uint8_t>(evt->buttons_b, 0x80);
+    Serial.println("");
+}
+
+
+
+New____________________  .cpp   =============================
+
+GamePadEventData JoystickEvents::mostRecentEvent;
+void JoystickEvents::OnGamePadChanged(const GamePadEventData *evt)
+{
+    mostReventEvent = *evt;
+}
